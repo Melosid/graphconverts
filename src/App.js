16cc0707857c600c3424487d45302cae39bdb407
melos <melosdauti7@gmail.com>
2021-02-04 22:26:25 +0100

    all matrix

--- src/App.js
+++ src/App.js
@@ -27,6 +27,10 @@ const App = () => {
   // const [finalLinksArray, setFinalLinksArray] = useState();
   const [modifiedLinks, setModifiedLinks] = useState();
 
+  const [initMatrix, setInitMatrix] = useState();
+  const [finalMatrix, setFinalMatrix] = useState();
+  const [differenceMatrix, setDifferenceMatrix] = useState();
+
   //
   //Extract text from .gr file and save it when .gr becomes available
   useEffect(() => {
@@ -82,6 +86,8 @@ const App = () => {
           matrix[parseInt(to) - 1][parseInt(from) - 1] = 1;
         }
       });
+
+      setInitMatrix(matrix);
 
       textByLine.forEach((line) => {
         var arrayOfLine = line.split(" ");
@@ -141,136 +147,179 @@ const App = () => {
 
       var pairsArray = [];
       textByLine.forEach((row) => {
-        // var nodeOnCluster = {
-        //   node: row[1],
-        //   cluster: row[lastIndex],
-        // };
+        var nodeOnCluster = {
+          node: row[1],
+          cluster: row[lastIndex],
+        };
 
-        // pairsArray.push(nodeOnCluster);
-        pairsArray.push(row[1]);
-        pairsArray.push(row[lastIndex]);
+        pairsArray.push(nodeOnCluster);
+        // pairsArray.push(row[1]);
+        // pairsArray.push(row[lastIndex]);
       });
 
       var finLinks = [];
-      // pairsArray.forEach((pairOne, indexOne) => {
-      //   pairsArray.forEach((pairTwo, indexTwo) => {
-      //     if (indexOne < indexTwo && pairOne.cluster === pairTwo.cluster) {
-      //       finLinks.push({
-      //         x: pairOne.node,
-      //         y: pairTwo.node,
-      //       });
-      //     }
-      //   });
-      // });
-      for (let i = 0; i < pairsArray.length; i += 2) {
-        for (let j = 0; j < pairsArray.length; j += 2) {
-          if (i < j && pairsArray[i + 1] === pairsArray[j + 1]) {
-            finLinks.push(pairsArray[i]);
-            finLinks.push(pairsArray[j]);
+      pairsArray.forEach((pairOne, indexOne) => {
+        pairsArray.forEach((pairTwo, indexTwo) => {
+          if (indexOne < indexTwo && pairOne.cluster === pairTwo.cluster) {
+            finLinks.push({
+              x: pairOne.node,
+              y: pairTwo.node,
+            });
           }
-        }
-      }
-      console.log(finLinks);
+        });
+      });
+      // for (let i = 0; i < pairsArray.length; i += 2) {
+      //   for (let j = 0; j < pairsArray.length; j += 2) {
+      //     if (i < j && pairsArray[i + 1] === pairsArray[j + 1]) {
+      //       finLinks.push(pairsArray[i]);
+      //       finLinks.push(pairsArray[j]);
+      //     }
+      //   }
+      // }
       setFinalLinks(finLinks);
+
+      //
+      //
+      //
+      //
+      var matrix = [];
+
+      for (let i = 0; i < dimensionNodes; i++) {
+        matrix.push([]);
+      }
+      matrix.forEach((row) => {
+        for (let i = 0; i < dimensionNodes; i++) {
+          row.push(0);
+        }
+      });
+      //
+      //
+      // console.log(finLinks);
+      finLinks.forEach((linkObj) => {
+        matrix[parseInt(linkObj.x) - 1][parseInt(linkObj.y) - 1] = 1;
+        matrix[parseInt(linkObj.y) - 1][parseInt(linkObj.x) - 1] = 1;
+      });
+      //
+      //
+      setFinalMatrix(matrix);
+      console.log("final matrix", matrix);
     }
   }, [textOfArffFile]);
-
+  //
+  //
   useEffect(() => {
-    if (initialLinks !== undefined && finalLinks !== undefined) {
-      var modifiedArray = [];
+    var matrix = [];
 
-      // initialLinks.forEach((linkInit) => {
-      //   var similar = finalLinks.find(
-      //     (linkFin) => linkFin.x === linkInit.x && linkFin.y === linkInit.y
-      //   );
-      //   if (similar === undefined) {
-      //     modifiedArray.push(linkInit);
-      //   }
-      // });
-
-      // finalLinks.forEach((linkFin) => {
-      //   var similar = initialLinks.find(
-      //     (linkInit) => linkInit.x === linkFin.x && linkInit.y === linkFin.y
-      //   );
-      //   if (similar === undefined) {
-      //     modifiedArray.push(linkFin);
-      //   }
-      // });
-      console.log("initial links ", initialLinks);
-      console.log("final links ", finalLinks);
-
-      console.log("initial links length", initialLinks.length);
-      console.log("final links length", finalLinks.length);
-
-      console.log("before first loop");
-      for (let l = 0; l < initialLinks.length; l += 2) {
-        var different = true;
-        for (let k = 0; k < finalLinks.length; k += 2) {
-          if (
-            initialLinks[l] === finalLinks[k] &&
-            initialLinks[l + 1] === finalLinks[k + 1]
-          ) {
-            different = false;
-          }
-        }
-        if (different) {
-          console.log(initialLinks[l], initialLinks[l + 1]);
-          modifiedArray.push(initialLinks[l]);
-          modifiedArray.push(initialLinks[l + 1]);
-        }
-      }
-      console.log("after first loop");
-
-      for (let p = 0; p < finalLinks.length; p += 2) {
-        var different2 = true;
-        for (let q = 0; q < initialLinks.length; q += 2) {
-          if (
-            finalLinks[p] === initialLinks[q] &&
-            finalLinks[p + 1] === initialLinks[q + 1]
-          ) {
-            different2 = false;
-          }
-        }
-        if (different2) {
-          modifiedArray.push(finalLinks[p]);
-          modifiedArray.push(finalLinks[p + 1]);
-        }
-      }
-      console.log("after second loop");
-
-      // for (var l = 0; l < 20; l++) {
-      //   for (var k = 0; k < 21; k++) {
-      //     if (!false) {
-      //       console.log("something from first", l, k);
-      //     }
-      //   }
-      // }
-      // console.log("after first loop");
-
-      // for (var p = 0; p < 20; p = p + 2) {
-      //   for (var q = 0; q < 21; q = q + 2) {
-      //     if (!false) {
-      //       console.log("something from second", p, q);
-      //     }
-      //   }
-      // }
-      // console.log("after second loop");
-
-      console.log("finished");
-      setModifiedLinks(modifiedArray);
-
-      // modifiedArray.forEach((modlink) => {
-      //   if (out.length === 0) {
-      //     out += `${modlink.x} ${modlink.y}`;
-      //   } else {
-      //     out += `\n${modlink.x} ${modlink.y}`;
-      //   }
-      // });
+    for (let i = 0; i < dimensionNodes; i++) {
+      matrix.push([]);
     }
-  }, [finalLinks]);
+    matrix.forEach((row) => {
+      for (let i = 0; i < dimensionNodes; i++) {
+        row.push(0);
+      }
+    });
+
+    for (let z = 0; z < dimensionNodes; z++) {
+      for (let x = 0; x < dimensionNodes; x++) {
+        if (initMatrix[z][x] !== finalMatrix[z][x]) {
+          matrix[z][x] = 1;
+        }
+      }
+    }
+    console.log("difference matrix", matrix);
+    setDifferenceMatrix(matrix);
+  }, [finalMatrix]);
+  //
+  //
+
+  // useEffect(() => {
+  //   if (initialLinks !== undefined && finalLinks !== undefined) {
+  //     var modifiedArray = [];
+
+  //     // initialLinks.forEach((linkInit) => {
+  //     //   var similar = finalLinks.find(
+  //     //     (linkFin) => linkFin.x === linkInit.x && linkFin.y === linkInit.y
+  //     //   );
+  //     //   if (similar === undefined) {
+  //     //     modifiedArray.push(linkInit);
+  //     //   }
+  //     // });
+
+  //     // finalLinks.forEach((linkFin) => {
+  //     //   var similar = initialLinks.find(
+  //     //     (linkInit) => linkInit.x === linkFin.x && linkInit.y === linkFin.y
+  //     //   );
+  //     //   if (similar === undefined) {
+  //     //     modifiedArray.push(linkFin);
+  //     //   }
+  //     // });
+
+  //     for (let l = 0; l < initialLinks.length; l += 2) {
+  //       var different = true;
+  //       for (let k = 0; k < finalLinks.length; k += 2) {
+  //         if (
+  //           initialLinks[l] === finalLinks[k] &&
+  //           initialLinks[l + 1] === finalLinks[k + 1]
+  //         ) {
+  //           different = false;
+  //         }
+  //       }
+  //       if (different) {
+  //         modifiedArray.push(initialLinks[l]);
+  //         modifiedArray.push(initialLinks[l + 1]);
+  //       }
+  //     }
+
+  //     for (let p = 0; p < finalLinks.length; p += 2) {
+  //       var different2 = true;
+  //       for (let q = 0; q < initialLinks.length; q += 2) {
+  //         if (
+  //           finalLinks[p] === initialLinks[q] &&
+  //           finalLinks[p + 1] === initialLinks[q + 1]
+  //         ) {
+  //           different2 = false;
+  //         }
+  //       }
+  //       if (different2) {
+  //         modifiedArray.push(finalLinks[p]);
+  //         modifiedArray.push(finalLinks[p + 1]);
+  //       }
+  //     }
+  //     console.log("after second loop");
+
+  //     // for (var l = 0; l < 20; l++) {
+  //     //   for (var k = 0; k < 21; k++) {
+  //     //     if (!false) {
+  //     //       console.log("something from first", l, k);
+  //     //     }
+  //     //   }
+  //     // }
+  //     // console.log("after first loop");
+
+  //     // for (var p = 0; p < 20; p = p + 2) {
+  //     //   for (var q = 0; q < 21; q = q + 2) {
+  //     //     if (!false) {
+  //     //       console.log("something from second", p, q);
+  //     //     }
+  //     //   }
+  //     // }
+  //     // console.log("after second loop");
+
+  //     console.log("finished");
+  //     setModifiedLinks(modifiedArray);
+
+  //     // modifiedArray.forEach((modlink) => {
+  //     //   if (out.length === 0) {
+  //     //     out += `${modlink.x} ${modlink.y}`;
+  //     //   } else {
+  //     //     out += `\n${modlink.x} ${modlink.y}`;
+  //     //   }
+  //     // });
+  //   }
+  // }, [finalLinks]);
 
   useEffect(() => {
-    if (modifiedLinks !== undefined) {
+    if (differenceMatrix !== undefined) {
       // var out = "";
       // modifiedLinks.forEach((modlink) => {
       //   if (out.length === 0) {
@@ -282,43 +331,56 @@ const App = () => {
       // console.log(out);
       // setOutputFile(out);
 
-      for (var h = 0; h < modifiedLinks.length; h += 2) {
-        for (var g = 0; g < modifiedLinks.length; g += 2) {
-          if (
-            modifiedLinks[h] === modifiedLinks[g] &&
-            modifiedLinks[h + 1] > modifiedLinks[g + 1]
-          ) {
-            // do nothing
-            console.log("sometthinggg");
-            var value3 = modifiedLinks[h + 1];
-            modifiedLinks[h + 1] = modifiedLinks[g + 1];
-            modifiedLinks[g + 1] = value3;
-          }
-          if (modifiedLinks[h] > modifiedLinks[g]) {
-            console.log("something else");
-            var value1 = modifiedLinks[h];
-            var value2 = modifiedLinks[h + 1];
-            modifiedLinks[h] = modifiedLinks[g];
-            modifiedLinks[h + 1] = modifiedLinks[g + 1];
-            modifiedLinks[g] = value1;
-            modifiedLinks[g + 1] = value2;
-          }
-        }
-      }
+      // var array = modifiedLinks;
+      // for (var h = 0; h < modifiedLinks.length; h += 2) {
+      //   for (var g = 0; g < modifiedLinks.length; g += 2) {
+      //     if (
+      //       modifiedLinks[h] === modifiedLinks[g] &&
+      //       modifiedLinks[h + 1] > modifiedLinks[g + 1]
+      //     ) {
+      //       // do nothing
+      //       console.log("sometthinggg");
+      //       var value3 = modifiedLinks[h + 1];
+      //       array[h + 1] = modifiedLinks[g + 1];
+      //       array[g + 1] = value3;
+      //     }
+      //     if (modifiedLinks[h] > modifiedLinks[g]) {
+      //       console.log("something else");
+      //       var value1 = modifiedLinks[h];
+      //       var value2 = modifiedLinks[h + 1];
+      //       array[h] = modifiedLinks[g];
+      //       array[h + 1] = modifiedLinks[g + 1];
+      //       array[g] = value1;
+      //       array[g + 1] = value2;
+      //     }
+      //   }
+      // }
 
       var out = "";
-      for (let r = 0; r < modifiedLinks.length; r += 2) {
-        if (out.length === 0) {
-          out += `${modifiedLinks[r]} ${modifiedLinks[r + 1]}`;
-        } else {
-          out += `\n${modifiedLinks[r]} ${modifiedLinks[r + 1]}`;
+      // for (let r = 0; r < modifiedLinks.length; r += 2) {
+      // if (out.length === 0) {
+      //   out += `${modifiedLinks[r]} ${modifiedLinks[r + 1]}`;
+      // } else {
+      //   out += `\n${modifiedLinks[r]} ${modifiedLinks[r + 1]}`;
+      // }
+      // }
+
+      for (let c = 0; c < dimensionNodes; c++) {
+        for (let v = 0; v < dimensionNodes; v++) {
+          if (c < v && differenceMatrix[c][v] === 1) {
+            if (out.length === 0) {
+              out += `${c + 1} ${v + 1}`;
+            } else {
+              out += `\n${c + 1} ${v + 1}`;
+            }
+          }
         }
       }
 
-      console.log(out);
+      // console.log(out);
       setOutputFile(out);
     }
-  }, [modifiedLinks]);
+  }, [differenceMatrix]);
 
   //
   //upload gr input file
